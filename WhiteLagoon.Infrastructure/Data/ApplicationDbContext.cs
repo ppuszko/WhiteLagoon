59bdd16ee4aabb849390849ef974cf2c0558f2af
Patryk Puszko <ppuszko3@gmail.com>
2024-10-16 19:17:43 +0200

    Added AppDbContext And configured Project Structure

---
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteLagoon.Infrastructure.Data {
+    internal class ApplicationDbContext : DbContext {
+        //the options instance with :base(options) passes all parameters from ApplicationDbContext to it's base class DbContext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){
+            
+        }
+    }
+}
