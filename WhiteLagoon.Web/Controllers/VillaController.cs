34a9d95ff88cd418b791c8e9bec7fddd395c33d7
Patryk Puszko <ppuszko3@gmail.com>
2024-10-17 16:39:14 +0200

    AddVillaIndex

---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers {
+    public class VillaController : Controller {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db) {
+            _db = db;
+        }
+        public IActionResult Index() {
+            //gets every object of Villas table and converts them to list
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+    }
+}
