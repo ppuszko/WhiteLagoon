3075122ea795e6c892dd244dffc6408799c27dd7
Patryk Puszko <ppuszko3@gmail.com>
2024-10-18 14:43:41 +0200

    Add Edit And Delete Functionality

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,17 @@ using System.Threading.Tasks;
 namespace WhiteLagoon.Domain.Entities {
     public class Villa {
         public int Id { get; set; }
+        [MaxLength(50)]
         public required string Name { get; set; }
         public string? Description { get; set; }
+        [Display(Name = "Price per night")]
+        [Range(10,1000)]
         public double Price { get; set; }
+        [Display(Name = "Square feet")]
         public int Sqft { get; set; }
+        [Range(1,10)]
         public int Occupancy { get; set; }
+        [Display(Name= "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? CreatedDate {get; set;}
         public DateTime? UpdatedDate { get; set; }
--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
 namespace WhiteLagoon.Web.Controllers {
@@ -11,6 +13,63 @@ namespace WhiteLagoon.Web.Controllers {
             //gets every object of Villas table and converts them to list
             var villas = _db.Villas.ToList();
             return View(villas);
+        }
+        public IActionResult Create() {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Villa obj) {
+            if (obj.Name == obj.Description) {
+                ModelState.AddModelError("Name", "The description can not exactly match the name");
+                
+            }
+            if (ModelState.IsValid) {
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Created succesfully!";
+                return RedirectToAction("Index", "Villa");
+            }
+            return View(obj);  
+        }
+
+        public IActionResult Edit(int? id) {
+            Villa? objToUpdate = _db.Villas.FirstOrDefault(u => u.Id == id);
+            if (objToUpdate == null) {
+                return RedirectToAction("Error", "Home");
+            }
+            
+            return View(objToUpdate);
+        }
+        [HttpPost]
+        public IActionResult Edit(Villa obj) {
+            if (ModelState.IsValid && obj.Id>0) {
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Edited succesfully!";
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id) {
+            Villa? objToDelete = _db.Villas.FirstOrDefault(u => u.Id == id);
+            if (objToDelete is null) {
+                return RedirectToAction("Error", "Home");
+            }
+            
+            return View(objToDelete);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id) {
+            Villa? objToDelete = _db.Villas.FirstOrDefault(u => u.Id == id);
+            if (objToDelete is not null) {
+                _db.Villas.Remove(objToDelete);
+                _db.SaveChanges();
+                TempData["success"] = "Deleted succesfully!";
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
         }
     }
 }
